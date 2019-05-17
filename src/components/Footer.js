10211df2e28679333a12c5f805676b373fede69b
Taylor Seamans <taylorseamans@Taylors-MacBook-Pro-2.local>
2019-05-17 14:57:17 -0700

    Removed Grommet from TopBanner and Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { 
 Box, 
-Grommet, 
 Anchor,
 Text,
 } from 'grommet';
--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { 
-Box,   
-Grommet, 
+Box,
 ResponsiveContext,
 Image,
 Grid,
